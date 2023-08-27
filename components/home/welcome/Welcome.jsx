1fa3c64ce15f1786e77eccfa97c7947777fbb75a
Jonathan <jfangon2002@gmail.com>
2023-08-26 18:21:59 -0700

    UI changes - functionality not there yet

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -12,8 +12,12 @@ import { useRouter } from 'expo-router';
 import styles from './welcome.style';
 import { icons, SIZES } from '../../../constants';
 
+const jobTypes = ['Full-time', 'Part-time', 'Contractor'];
+
 const Welcome = () => {
   const router = useRouter();
+
+  const [activeJobType, setActiveJobType] = useState('Full-time');
 
   return (
     <View>
@@ -31,6 +35,34 @@ const Welcome = () => {
             placeholder='What are you looking for?'
           />
         </View>
+
+        <TouchableOpacity style={styles.searchBtn} onPress={() => {}}>
+          <Image
+            source={icons.search}
+            resizeMode='contain'
+            style={styles.searchBtnImage}
+          />
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.tabsContainer}>
+        <FlatList
+          data={jobTypes}
+          renderItem={({ item }) => (
+            <TouchableOpacity
+              style={styles.tab(activeJobType, item)}
+              onPress={() => {
+                setActiveJobType(item);
+                router.push(`/search/${item}`);
+              }}
+            >
+              <Text style={styles.tabText(activeJobType, item)}>{item}</Text>
+            </TouchableOpacity>
+          )}
+          keyExtractor={(item) => item}
+          contentContainerStyle={{ columnGap: SIZES.small }}
+          horizontal
+        />
       </View>
     </View>
   );
