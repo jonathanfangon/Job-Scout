a7f39882d64bffbe4bfab8d8529bc3e26621d334
Jonathan <jfangon2002@gmail.com>
2023-08-28 16:31:00 -0700

    finished UI for the home page - images seems to be a bit weird as of now FYI

--- components/common/cards/nearby/nearbyjobcard.style.js
+++ components/common/cards/nearby/nearbyjobcard.style.js
@@ -1,16 +1,16 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet } from 'react-native';
 
-import { COLORS, SHADOWS, SIZES } from "../../../../constants";
+import { COLORS, SHADOWS, SIZES } from '../../../../constants';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "space-between",
-    alignItems: "center",
-    flexDirection: "row",
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    flexDirection: 'row',
     padding: SIZES.medium,
     borderRadius: SIZES.small,
-    backgroundColor: "#FFF",
+    backgroundColor: '#FFF',
     ...SHADOWS.medium,
     shadowColor: COLORS.white,
   },
@@ -19,12 +19,12 @@ const styles = StyleSheet.create({
     height: 50,
     backgroundColor: COLORS.white,
     borderRadius: SIZES.medium,
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  logImage: {
-    width: "70%",
-    height: "70%",
+  logoImage: {
+    width: '70%',
+    height: '70%',
   },
   textContainer: {
     flex: 1,
@@ -32,15 +32,15 @@ const styles = StyleSheet.create({
   },
   jobName: {
     fontSize: SIZES.medium,
-    fontFamily: "DMBold",
+    fontFamily: 'DMBold',
     color: COLORS.primary,
   },
   jobType: {
     fontSize: SIZES.small + 2,
-    fontFamily: "DMRegular",
+    fontFamily: 'DMRegular',
     color: COLORS.gray,
     marginTop: 3,
-    textTransform: "capitalize",
+    textTransform: 'capitalize',
   },
 });
 
--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -21,6 +21,13 @@ const Popularjobs = () => {
     num_pages: 1,
   });
 
+  const [selectedJob, setSelectedJob] = useState();
+
+  const handleCardPress = (item) => {
+    router.push(`/job-details/${item.job_id}`);
+    setSelectedJob(item.job_id);
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
@@ -38,7 +45,13 @@ const Popularjobs = () => {
         ) : (
           <FlatList
             data={data}
-            renderItem={({ item }) => <PopularJobCard item={item} />}
+            renderItem={({ item }) => (
+              <PopularJobCard
+                item={item}
+                selectedJob={selectedJob}
+                handleCardPress={handleCardPress}
+              />
+            )}
             keyExtractor={(item) => item?.job_id}
             contentContainerStyle={{ columnGap: SIZES.medium }}
             horizontal
